32bb857359267ccc4fe1d1fb18810e834adaf2aa
Bob Poekert <bob@factual.com>
2015-05-15 18:46:21 +0000

    update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
 (defproject lein-hadoop-cluster "0.1.5-SNAPSHOT"
+  :repositories [["factual" {:url      "http://maven.corp.factual.com/nexus/content/groups/public/"
+                             :snapshot {:update :always}}]]
+  :deploy-repositories [["snapshots" {:url           "http://maven.corp.factual.com/nexus/content/repositories/snapshots"
+                                      :sign-releases false}]
+                        ["releases" {:url           "http://maven.corp.factual.com/nexus/content/repositories/releases"
+                                     :sign-releases false}]]
   :description "Run Leiningen tasks against a live Hadoop cluster."
   :url "http://github.com/llasram/lein-hadoop-cluster"
   :license {:name "Eclipse Public License"
